93c0da44f22524af52e7466bc7fbe0eb6116e0a7
Eugene Reich <eugene.reich@gmail.com>
2016-01-09 12:09:46 +0400

    Updated assert array type

---
+++ source/Assert/ArrayType.php
@@ -0,0 +1,63 @@
+<?php namespace Apishka\Validator\Assert;
+
+use Apishka\Validator\AssertAbstract;
+
+/**
+ * Array type
+ *
+ * @uses AssertAbstract
+ *
+ * @author Evgeny Reykh <evgeny@reykh.com>
+ */
+
+class ArrayType extends AssertAbstract
+{
+    /**
+     * Get supported names
+     *
+     * @return array
+     */
+
+    public function getSupportedNames()
+    {
+        return array(
+            'Assert/Array',
+        );
+    }
+
+    /**
+     * Process
+     *
+     * @param mixed $value
+     * @param array $options
+     *
+     * @return string|null
+     */
+
+    public function process($value, array $options = array())
+    {
+        if ($value === null)
+            return;
+
+        if (!is_array($value))
+            $this->throwException($options, 'error');
+
+        return $value;
+    }
+
+    /**
+     * Get default errors
+     *
+     * @return array
+     */
+
+    protected function getDefaultErrors()
+    {
+        return array(
+            'error' => array(
+                'message'   => 'wrong input format',
+            ),
+        );
+    }
+}
+
