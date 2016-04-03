9b74f7f9fa30174329c32253db016e3ecb06e8c3
Stas <user.cryo@gmail.com>
2016-04-03 12:06:57 +0300

    Add testing ArrayType,FloatType,PostFileArray,StringType. Add checking index in PostFileArray.

---
+++ tests/Assert/PostFileArrayTest.php
@@ -0,0 +1,93 @@
+<?php namespace ApishkaTest\Validator\Assert;
+
+use Apishka\Validator\Assert\PostFileArray;
+
+class PostFileArrayTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Prepare assert
+     *
+     * @return PostFileArray
+     */
+
+    protected function prepareAssert()
+    {
+        return new PostFileArray();
+    }
+
+    /**
+     * Test valid data
+     */
+
+    public function testValidData()
+    {
+        $this->assertSame(
+            array('error' => UPLOAD_ERR_OK),
+            $this->prepareAssert()->process(array('error' => UPLOAD_ERR_OK))
+        );
+    }
+
+    /**
+     * Test null
+     */
+
+    public function testNull()
+    {
+        $this->assertSame(
+            null,
+            $this->prepareAssert()->process(null)
+        );
+    }
+
+    /**
+     * Test wrong values
+     * @dataProvider             wrongValuesProvider
+     * @expectedException        \Apishka\Validator\Exception
+     * @expectedExceptionMessage  wrong input format
+     */
+
+    public function testWrongValues($wrong_type)
+    {
+        $assert = $this->prepareAssert();
+        $assert->process($wrong_type);
+    }
+
+    /**
+     * Test wrong values
+     * @expectedException        \Apishka\Validator\Exception
+     * @expectedExceptionMessage  upload
+     */
+
+    public function testEmptyUpload()
+    {
+        $assert = $this->prepareAssert();
+        $assert->process(array());
+    }
+
+    /**
+     * Test wrong values
+     * @expectedException        \Apishka\Validator\Exception
+     * @expectedExceptionMessage  upload
+     */
+
+    public function testWrongUpload()
+    {
+        $assert = $this->prepareAssert();
+        $assert->process(array('error' => !UPLOAD_ERR_OK));
+    }
+
+    /**
+     * Wrong data provider
+     * @return array
+     */
+
+    public function wrongValuesProvider()
+    {
+        return array(
+            array('test'),
+            array(new \StdClass()),
+            array(true),
+            array(2),
+        );
+    }
+}
