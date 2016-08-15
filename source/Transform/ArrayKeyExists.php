0109079b8bdf253609d60d753e75e0aa052d37cf
Eugene Reich <eugene.reich@gmail.com>
2016-08-16 00:55:46 +0400

    Added array key exists test

---
+++ source/Transform/ArrayKeyExists.php
@@ -0,0 +1,67 @@
+<?php namespace Apishka\Transformer\Transform;
+
+use Apishka\Transformer\TransformAbstract;
+
+/**
+ * Array key exists
+ */
+
+class ArrayKeyExists extends TransformAbstract
+{
+    /**
+     * Get supported names
+     *
+     * @return array
+     */
+
+    public function getSupportedNames()
+    {
+        return array(
+            'Transform/ArrayKeyExists',
+        );
+    }
+
+    /**
+     * Process
+     *
+     * @param mixed $value
+     * @param array $options
+     *
+     * @return mixed
+     */
+
+    public function process($value, array $options = array())
+    {
+        if ($value === null)
+            return;
+
+        if (is_object($value) || is_resource($value) || is_array($value))
+            $this->throwException($options, 'error');
+
+        if (!array_key_exists('values', $options))
+            throw new \InvalidArgumentException('Property "values" not found in options');
+
+        if (!is_array($options['values']))
+            throw new \InvalidArgumentException('Property "values" is not function');
+
+        if (!array_key_exists((string) $value, $options['values']))
+            $this->throwException($options, 'error');
+
+        return $value;
+    }
+
+    /**
+     * Get default errors
+     *
+     * @return array
+     */
+
+    protected function getDefaultErrors()
+    {
+        return array(
+            'error' => array(
+                'message'   => 'wrong input format',
+            ),
+        );
+    }
+}
---
+++ tests/Transform/ArrayKeyExistsTest.php
@@ -0,0 +1,114 @@
+<?php namespace ApishkaTest\Transformer\Transform;
+
+use Apishka\Transformer\Transform\ArrayKeyExists;
+
+/**
+ * Array key exists test
+ */
+
+class ArrayKeyExistsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Prepare assert
+     *
+     * @return ArrayKeyExists
+     */
+
+    protected function prepareAssert()
+    {
+        return new ArrayKeyExists($options);
+    }
+
+    /**
+     * Test array
+     */
+
+    public function testArray()
+    {
+        $this->assertSame(
+            10,
+            $this->prepareAssert()->process(10, ['values' => [10 => null]])
+        );
+    }
+
+    /**
+     * Test null
+     */
+
+    public function testNull()
+    {
+        $this->assertSame(
+            null,
+            $this->prepareAssert()->process(null)
+        );
+    }
+
+    /**
+     * Test wrong types
+     *
+     * @dataProvider             wrongValuesProvider
+     * @expectedException        \Apishka\Transformer\Exception
+     * @expectedExceptionMessage wrong input format
+     *
+     * @param mixed $value
+     * @param array $values
+     */
+
+    public function testWrongValues($value, array $values)
+    {
+        $assert = $this->prepareAssert();
+        $assert->process($value, ['values' => $values]);
+    }
+
+    /**
+     * Wrong data provider
+     *
+     * @return array
+     */
+
+    public function wrongValuesProvider()
+    {
+        return array(
+            array(1, ['test' => 'test']),
+            array(1.2, ['test' => 'test']),
+            array(true, ['test' => 'test']),
+            array('test', ['test1' => 'test1']),
+            array(function () {}, ['test' => 'test']),
+            array(new \StdClass(), ['test' => 'test']),
+        );
+    }
+
+    /**
+     * Test good values
+     *
+     * @dataProvider             goodValuesProvider
+     *
+     * @param mixed $value
+     * @param array $values
+     */
+
+    public function testGoodValues($value, $values)
+    {
+        $assert = $this->prepareAssert();
+        $this->assertSame(
+            $value,
+            $assert->process($value, ['values' => $values])
+        );
+    }
+
+    /**
+     * Good data provider
+     *
+     * @return array
+     */
+
+    public function goodValuesProvider()
+    {
+        return array(
+            array(1, ['1' => 'test']),
+            array(1.2, ['1.2' => 'test']),
+            array(true, ['1' => 'test']),
+            array('test', ['test' => 'test1']),
+        );
+    }
+}
