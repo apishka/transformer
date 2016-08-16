328ab08f5dee58607f33324825755f07fe42bced
Alex Lobtsov <dessander@gmail.com>
2016-08-16 13:43:33 +0400

    Added Phone Transform

---
+++ source/Transform/Phone.php
@@ -0,0 +1,110 @@
+<?php namespace Apishka\Transformer\Transform;
+
+use Apishka\Transformer\TransformAbstract;
+
+/**
+ * Phone type
+ */
+
+class Phone extends TransformAbstract
+{
+    /**
+     * Get supported names
+     *
+     * @return array
+     */
+
+    public function getSupportedNames()
+    {
+        return array(
+            'Transform/Phone',
+        );
+    }
+
+    /**
+     * Process
+     *
+     * @param mixed $value
+     * @param array $options
+     *
+     * @return string|null
+     */
+
+    public function process($value, array $options = array())
+    {
+        if ($value === null)
+            return;
+
+        if (!class_exists('\libphonenumber\PhoneNumberUtil'))
+            throw new RuntimeException('Library for phone checking not found');
+
+        if (is_object($value) || is_resource($value) || is_array($value))
+            $this->throwException($options, 'error');
+
+        if (!preg_match('#^\+#', $value))
+            $value = '+' . $value;
+
+        $util = \libphonenumber\PhoneNumberUtil::getInstance();
+
+        try
+        {
+            $phone = $util->parse($value, $this->getDefaultCountryCode($options));
+        }
+        catch(\libphonenumber\NumberParseException $e)
+        {
+            $this->throwException($options, 'error');
+        }
+
+        if (!$util->isValidNumber($phone))
+            $this->throwException($options, 'error');
+
+        return (string) $util->format($phone, $this->getDefaultPhoneFormat($options));
+    }
+
+    /**
+     * Get default errors
+     *
+     * @return array
+     */
+
+    protected function getDefaultErrors()
+    {
+        return array(
+            'error' => array(
+                'message'   => 'wrong phone format',
+            ),
+        );
+    }
+
+    /**
+     * Get default country code
+     *
+     * @param array $options
+     *
+     * @return string
+     */
+
+    protected function getDefaultCountryCode($options)
+    {
+        if (!isset($options['country_code']))
+            throw new \InvalidArgumentException('`country_code` must be defined in options');
+
+        return $options['country_code'];
+    }
+
+    /**
+     * Get default phone format
+     *
+     * @param array $options
+     *
+     * @return string
+     */
+
+    protected function getDefaultPhoneFormat($options)
+    {
+        if (!isset($options['phone_format']))
+            return \libphonenumber\PhoneNumberFormat::E164;
+
+        return $options['phone_format'];
+    }
+}
---
+++ tests/Transform/PhoneTest.php
@@ -0,0 +1,139 @@
+<?php namespace ApishkaTest\Transformer\Transform;
+
+use Apishka\Transformer\Transform\Phone;
+
+/**
+ * Phone assert test
+ */
+
+class PhoneTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Prepare assert
+     *
+     * @return Phone
+     */
+
+    protected function prepareAssert()
+    {
+        return new Phone();
+    }
+
+    /**
+     * Test null
+     */
+
+    public function testNull()
+    {
+        $this->assertNull(
+            $this->prepareAssert()->process(null)
+        );
+    }
+
+    /**
+     * Test object
+     *
+     * @expectedException        \Apishka\Transformer\Exception
+     * @expectedExceptionMessage wrong phone format
+     */
+
+    public function testObject()
+    {
+        $this->prepareAssert()->process(new \StdClass());
+    }
+
+    /**
+     * Test array
+     *
+     * @expectedException        \Apishka\Transformer\Exception
+     * @expectedExceptionMessage wrong phone format
+     */
+
+    public function testArray()
+    {
+        $this->prepareAssert()->process(array(1));
+    }
+    
+    /**
+     * Test empty country code
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage `country_code` must be defined in options
+     */
+
+    public function testEmptyCountryCode()
+    {
+        $this->prepareAssert()->process(
+            '+79161234567'
+        );
+    }
+
+    /**
+     * Test simple phone
+     */
+
+    public function testSimplePhone()
+    {
+        $this->assertSame(
+            '+79161234567',
+            $this->prepareAssert()->process(
+                '+79161234567',
+                array(
+                    'country_code' => 'RU',
+                )
+            )
+        );
+    }
+
+    /**
+     * Test simple phone
+     */
+
+    public function testSimplePhoneWithoutPlus()
+    {
+        $this->assertSame(
+            '+79161234567',
+            $this->prepareAssert()->process(
+                '79161234567',
+                array(
+                    'country_code' => 'RU',
+                )
+            )
+        );
+    }
+    
+    /**
+     * Test another phone format
+     */
+
+    public function testAnotherPhoneFormat()
+    {
+        $this->assertSame(
+            '+7 916 123-45-67',
+            $this->prepareAssert()->process(
+                '79161234567',
+                array(
+                    'country_code' => 'RU',
+                    'phone_format' => \libphonenumber\PhoneNumberFormat::INTERNATIONAL,
+                )
+            )
+        );
+    }
+
+    /**
+     * Test simple phone
+     *
+     * @expectedException        \Apishka\Transformer\Exception
+     * @expectedExceptionMessage wrong phone format
+     */
+
+    public function testNotValidPhone()
+    {
+        $this->prepareAssert()->process(
+            'some text',
+            array(
+                'country_code' => 'RU',
+            )
+        );
+    }
+}
