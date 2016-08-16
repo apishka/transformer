5ef08fcb5a77de41dbf0b7f2b9eff1a03c7974f1
Eugene Reich <eugene.reich@gmail.com>
2016-08-16 16:54:59 +0400

    Added tests on upper

---
+++ tests/Transform/UpperTest.php
@@ -0,0 +1,84 @@
+<?php namespace ApishkaTest\Transformer\Transform;
+
+use Apishka\Transformer\Transform\Upper;
+
+/**
+ * Upper transform test
+ */
+
+class UpperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Prepare assert
+     *
+     * @return Lower
+     */
+
+    protected function prepareSanitizer()
+    {
+        return new Upper();
+    }
+
+    /**
+     * Test null
+     */
+
+    public function testNull()
+    {
+        $this->assertNull(
+            $this->prepareSanitizer()->process(null)
+        );
+    }
+
+    /**
+     * Test string
+     */
+
+    public function testString()
+    {
+        $this->assertSame(
+            '10',
+            $this->prepareSanitizer()->process(10)
+        );
+
+        $this->assertSame(
+            'TEST',
+            $this->prepareSanitizer()->process('tEsT')
+        );
+
+        $this->assertSame(
+            '',
+            $this->prepareSanitizer()->process('')
+        );
+    }
+
+    /**
+     * Test wrong values
+     *
+     * @dataProvider             wrongValuesProvider
+     * @expectedException        \Apishka\Transformer\Exception
+     * @expectedExceptionMessage wrong input format
+     */
+
+    public function testWrongValues($wrong_type)
+    {
+        $assert = $this->prepareSanitizer();
+        $assert->process($wrong_type);
+    }
+
+    /**
+     * Wrong data provider
+     *
+     * @return array
+     */
+
+    public function wrongValuesProvider()
+    {
+        return array(
+            array(array()),
+            array(STDOUT),
+            array(function () {}),
+            array(new \StdClass()),
+        );
+    }
+}
