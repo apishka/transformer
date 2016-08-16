4f7d4882b491f1b64d58381b9e94a2b869809a2a
Eugene Reich <eugene.reich@gmail.com>
2016-08-16 16:35:39 +0400

    Added transformer to upper

---
+++ source/Transform/Upper.php
@@ -0,0 +1,61 @@
+<?php namespace Apishka\Transformer\Transform;
+
+use Apishka\Transformer\TransformAbstract;
+
+/**
+ * Lower
+ */
+
+class Upper extends TransformAbstract
+{
+    /**
+     * Get supported names
+     *
+     * @return array
+     */
+
+    public function getSupportedNames()
+    {
+        return array(
+            'Transform/Upper',
+        );
+    }
+
+    /**
+     * Process
+     *
+     * @param mixed $value
+     * @param array $options
+     *
+     * @return mixed
+     */
+
+    public function process($value, array $options = array())
+    {
+        if ($value === null)
+            return;
+
+        if (is_object($value) || is_resource($value) || is_array($value))
+            $this->throwException($options, 'error');
+
+        if (function_exists('mb_strtoupper'))
+            return mb_strtoupper($value);
+
+        return strtoupper($value);
+    }
+
+    /**
+     * Get default errors
+     *
+     * @return array
+     */
+
+    protected function getDefaultErrors()
+    {
+        return array(
+            'error' => array(
+                'message'   => 'wrong input format',
+            ),
+        );
+    }
+}
