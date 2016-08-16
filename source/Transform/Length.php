4b003b42373cb87f1fb38e79467f6e0e2baf0451
Eugene Reich <eugene.reich@gmail.com>
2016-08-16 16:50:01 +0400

    Added transform length

---
+++ source/Transform/Length.php
@@ -0,0 +1,78 @@
+<?php namespace Apishka\Transformer\Transform;
+
+use Apishka\Transformer\TransformAbstract;
+
+/**
+ * Not blank
+ */
+
+class Length extends TransformAbstract
+{
+    /**
+     * Get supported names
+     *
+     * @return array
+     */
+
+    public function getSupportedNames()
+    {
+        return array(
+            'Transform/Length',
+        );
+    }
+
+    /**
+     * Process
+     *
+     * @param mixed $value
+     * @param array $options
+     *
+     * @return mixed
+     */
+
+    public function process($value, array $options = array())
+    {
+        if ($value === null)
+            return;
+
+        if (is_object($value) || is_resource($value) || is_array($value))
+            $this->throwException($options, 'error');
+
+        if (!isset($options['min']) && !isset($options['max']))
+            throw new InvalidArgumentException('Not found "min" or "max" not found in options');
+
+        $length = function_exists('mb_strlen')
+            ? mb_strlen($value)
+            : strlen($value)
+        ;
+
+        if (isset($options['min']) && $length < $options['min'])
+            $this->throwException($options, 'error_min', ['count' => $options['min'], 'length' => $length]);
+
+        if (isset($options['max']) && $length > $options['max'])
+            $this->throwException($options, 'error_max', ['count' => $options['min'], 'length' => $length]);
+
+        return $value;
+    }
+
+    /**
+     * Get default errors
+     *
+     * @return array
+     */
+
+    protected function getDefaultErrors()
+    {
+        return array(
+            'error' => array(
+                'message'   => 'wrong input format',
+            ),
+            'error_min' => array(
+                'message'   => 'min {count} characters',
+            ),
+            'error_max' => array(
+                'message'   => 'max {count} characters',
+            ),
+        );
+    }
+}
